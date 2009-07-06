05e38d546c9ff65949c05fd14e2b6d25cec3546b
Mike McCarty <mmccarty@nrao.edu>
2009-07-06 16:30:49 -0400

    added ProjectType

---
+++ src/edu/nrao/dss/client/ProjectType.java
@@ -0,0 +1,24 @@
+package edu.nrao.dss.client;
+
+import com.extjs.gxt.ui.client.data.ModelType;
+
+public class ProjectType extends ModelType {
+	public ProjectType() {
+		setRoot("projects");
+		setTotalName("total");
+		
+		addField("id");
+		addField("pcode");
+		addField("type");
+		addField("semester");
+		addField("name");
+		addField("total_time");
+		addField("PSC_time");
+		addField("sem_time");
+		addField("grade");
+		addField("thesis");
+		addField("complete");
+		addField("ignore_grade");
+	}
+
+}
