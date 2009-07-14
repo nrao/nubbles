fe38d52a210b69a0efb824eb139cceee70225c33
Paul Marganian <pmargani@nrao.edu>
2009-07-14 07:00:02 -0400

    now schedule calendar is loading real periods

--- src/edu/nrao/dss/client/ScheduleCalendar.java
+++ src/edu/nrao/dss/client/ScheduleCalendar.java
@@ -2,40 +2,43 @@ package edu.nrao.dss.client;
 
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 import com.extjs.gxt.ui.client.Style.HorizontalAlignment;
+import com.extjs.gxt.ui.client.event.BaseEvent;
+import com.extjs.gxt.ui.client.event.ButtonEvent;
+import com.extjs.gxt.ui.client.event.Events;
+import com.extjs.gxt.ui.client.event.Listener;
+import com.extjs.gxt.ui.client.event.SelectionListener;
 
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.form.LabelField;
 import com.extjs.gxt.ui.client.widget.layout.TableData;
 import com.extjs.gxt.ui.client.widget.layout.TableLayout;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Widget;
+
 
 public class ScheduleCalendar extends ContentPanel { 
 
-	// temporary class used for populating calendar table
-    public class CalendarRow {
-    	public String start; // datetime
-    	public String duration; // minutes
-    	public String session;
-    	
-		public CalendarRow(String st, String dur, String sess) {
-    		start = st;
-    		duration = dur;
-    		session = sess;
-    	}
-    }
+	private Date startCalendarDay = new Date();
+	private int numCalendarDays = 1;
 	
-	public ScheduleCalendar() {
+	public ScheduleCalendar(Date start, int days) {
 			super();
-			initLayout();
+			startCalendarDay = start;
+			numCalendarDays = days;
+			initLayout(start, days);
 	}	
 	
 	@SuppressWarnings("unchecked") 
-	protected void initLayout() {
+	protected void initLayout(Date start, int days) {
 		
 		setHeading("West: Calendar");
 		setBorders(true);
@@ -47,22 +50,21 @@ public class ScheduleCalendar extends ContentPanel {
 		tl.setBorder(1);
 		setLayout(tl);
 
-		// fake the calendar data
-		List<CalendarRow> periods = new ArrayList<CalendarRow>();
-		periods.add(new CalendarRow("2009-06-01 00:00:00", "60", "Session(8): Cygnis X-1"));
-		periods.add(new CalendarRow("2009-06-01 01:00:00", "60", "Session(34): Uranus"));
-		
-		populateCalendar(periods);
+		// get the data to list
+		loadPeriods(start, days);
 	}
 	
-	protected void populateCalendar(List<CalendarRow> periods) {
-		for (CalendarRow period : periods) {
-			addButton("Edit");
-			addButton("Delete");
-			addLabel(period.start);
-			addLabel(period.duration);
-			addLabel(period.session);
-		}
+	protected void populateCalendar() {
+		// TBF: need to clear previously created
+		// table data
+		///this.removeAll();
+	    for (Period period : periods) {
+	    	addButton("Edit");
+	    	addDeleteButton(period);
+	    	addLabel(period.getStartString());
+	    	addLabel(Integer.toString(period.getDuration()));
+	    	addLabel(period.getSessionLabel());
+	    }
 	}
 	
 	@SuppressWarnings("unchecked") 
@@ -73,10 +75,54 @@ public class ScheduleCalendar extends ContentPanel {
 	}
 	
 	@SuppressWarnings("unchecked") 
+	protected void addDeleteButton(Period p){
+		TableData td = new TableData();
+		td.setHorizontalAlign(HorizontalAlignment.CENTER);
+		Button b = new Button("Delete");
+		b.setItemId(Integer.toString(p.getId()));
+		b.addSelectionListener(new SelectionListener<ButtonEvent>() {
+			@Override
+			public void componentSelected(ButtonEvent be) {
+	    		Button b = (Button) be.getSource();
+				String id = b.getItemId();
+				JSONRequest.delete("/periodJSON/" + id,
+						new JSONCallbackAdapter() {
+							public void onSuccess(JSONObject json) {
+					    		loadPeriods(startCalendarDay, numCalendarDays);
+							}
+						});
+			}
+		});		
+
+		add((Widget) b);
+	}	
+	@SuppressWarnings("unchecked") 
 	protected void addLabel(String data){
 		TableData td = new TableData();
 		td.setHorizontalAlign(HorizontalAlignment.CENTER);
 		add((Widget) new LabelField(data), td);	
 	}
+	
+	public void loadPeriods(Date start, int numDays) {
+		String startStr = "2009-06-01"; //DateTimeFormat.getFormat("yyy-MM-dd").format(start);
+		String uri = "/periodJSON?startPeriods=" + startStr + "&daysPeriods=" + Integer.toString(numDays);
+		JSONRequest.getWithKeywords(uri, new JSONCallbackAdapter() {
+			@Override
+		    public void onSuccess(JSONObject json) {
+		        periods = new ArrayList<Period>();
+		        JSONArray ps = json.get("periods").isArray();
+		        for (int i = 0; i < ps.size(); ++i) {
+		        	Period period = Period.parseJSON(ps.get(i).isObject());
+		        	if (period != null) {
+		        		periods.add(period);
+		        	}
+		        }
+		        populateCalendar();	
+		    }
+		});
+	}
+	
+    private List<Period> periods;
+
 }	
 	
