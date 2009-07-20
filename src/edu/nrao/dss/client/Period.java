3eed428db96e9af8d9f0812e9f9724e225b03d64
Paul Marganian <pmargani@nrao.edu>
2009-07-20 07:10:33 -0400

    first take at incorporating gwt-cal for a read-only schedule calendar

--- src/edu/nrao/dss/client/Period.java
+++ src/edu/nrao/dss/client/Period.java
@@ -1,41 +1,65 @@
 package edu.nrao.dss.client;
 
+// WTF: I can't fucking use this!!!
+//import java.util.GregorianCalendar;
+import java.util.Date;
+
+
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.json.client.JSONObject;
 
 public class Period {
 	
-    //private static final DateTimeFormat DATE_FORMAT = DateTimeFormat.getFormat("yyyy-MM-dd HH:mm:ss");
-	
-    // TBF
+    private static final DateTimeFormat DATE_FORMAT = DateTimeFormat.getFormat("yyyy-MM-dd HH:mm:ss");
+
     public static Period parseJSON(JSONObject json) {
     	//try {
             int id = (int) json.get("id").isNumber().doubleValue();
-            String desc = "Session"; //int2str(id); TBF: must get session name
-            // TBF: start should be a datetime
-            String stStr = json.get("start").isString().stringValue();
-            //Calendar st = Calendar.getInstance(); //DATE_FORMAT.parse(stStr);
-            int dur = 180;
-            Period period = new Period(id, desc, stStr, dur);
+            String handle = (String) json.get("handle").isString().stringValue();
+            Date st = DATE_FORMAT.parse(json.get("start").isString().stringValue());
+            int dur = (int) hours2minutes(json.get("duration").isNumber().doubleValue());
+            Period period = new Period(id, handle, st, dur);
             return period;
     	//} catch (NullPointerException e) {
         //	return null;
     		
     	//}
     }
-    
-    public Period(int id, String desc, String st, int dur) {
+
+    private static double hours2minutes(double hours) {
+    	return hours * 60.0;
+    }
+    public Period(int id, String handle, Date st, int dur) {
     	this.id = id;
-    	this.sessionDesc = desc;
-    	this.start = st; // TBF - should be datetime
+    	this.handle = handle;
+    	this.start = st; 
     	this.duration = dur;
     }
     
-    public String getSessionLabel() {
-    	return sessionDesc + "(" + Integer.toString(id) + ")";
+    public String getHandle() {
+    	return handle;
+    }
+    
+    public Date getStart() {
+    	return start;
+    }
+    
+    public Date getEnd() {
+        // WTF: time arithmetic should not be this hard!
+    	// WTF: can't use GregorianCalendar in GWT!
+//    	GregorianCalendar cal = new GregorianCalendar(1872, GregorianCalendar.OCTOBER, 2); //GregorianCalendar.getInstance();
+//    	cal.setTime(start);
+//    	int durMins = (int) ((int) duration * 60.0);
+//    	cal.add(GregorianCalendar.MINUTE, durMins);
+//    	return cal.getTime();
+    	long startSecs = start.getTime();
+    	// add the duration (in minutes) to this time in milli-seconds
+    	long endSecs = (long) (startSecs + (duration * 60.0 * 1000.0));
+    	return new Date(endSecs);
     }
     
     public String getStartString() {
-    	return start; //start.toString(); // TBF
+    	return start.toString(); // TBF
     }
     
     public int getDuration() {
@@ -47,10 +71,7 @@ public class Period {
     }
     
     private int      id;
-    private String   sessionDesc; // TBF - link to session obj?
-    //private Calendar start;
-    // TBF - I don't understand how to deal w/ date times
-    // in java!!!!
-    private String   start;
+    private String   handle; 
+    private Date     start;
     private int      duration; // minutes
 }
