ebf9edcf93f4d567a2c64415ecf5bce560de2d44
Paul Marganian <pmargani@nrao.edu>
2009-11-13 05:32:58 -0500

    new ProjectPage allows editing of project note fields, and is set up for further field editing

---
+++ src/edu/nrao/dss/client/ProjectPage.java
@@ -0,0 +1,252 @@
+package edu.nrao.dss.client;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+import com.extjs.gxt.ui.client.Style.Orientation;
+import com.extjs.gxt.ui.client.event.BaseEvent;
+import com.extjs.gxt.ui.client.event.Events;
+import com.extjs.gxt.ui.client.event.Listener;
+import com.extjs.gxt.ui.client.widget.ContentPanel;
+import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.form.FormPanel;
+import com.extjs.gxt.ui.client.widget.form.SimpleComboBox;
+import com.extjs.gxt.ui.client.widget.form.TextArea;
+import com.extjs.gxt.ui.client.widget.form.TextField;
+import com.extjs.gxt.ui.client.widget.layout.FlowLayout;
+import com.extjs.gxt.ui.client.widget.layout.FormData;
+import com.extjs.gxt.ui.client.widget.layout.RowLayout;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONObject;
+
+
+public class ProjectPage extends ContentPanel {
+
+	private FormPanel projectForm = new FormPanel();	
+	private SimpleComboBox<String> projects = new SimpleComboBox<String>();
+	private TextField<String> name = new TextField<String>();
+	private TextField<String> pi   = new TextField<String>();
+	private TextField<String> coi  = new TextField<String>();
+	private TextArea schNotes = new TextArea();
+	private TextArea obsNotes = new TextArea();
+	private Button save = new Button();
+	private Button reset = new Button();
+	
+    private HashMap<String, Integer> project_ids = new HashMap<String, Integer>();
+    private JSONObject projectJson;
+    private FormData fd = new FormData(500, 25);
+	
+	public ProjectPage() {
+		initLayout();
+		initListeners();
+	}
+	
+	private void initLayout() {
+
+		setLayout(new RowLayout(Orientation.VERTICAL));
+		
+
+		setBorders(false);
+		setHeaderVisible(false);
+		
+		// so we can always see everything 
+		//setScrollMode(Scroll.ALWAYS); 
+
+		//projectForm.setHeading("Project");
+		//projectForm.setBorders(true);
+        projectForm.setHeaderVisible(false);
+		
+		// the project picker goes in this left-most form panel
+		projects.setFieldLabel("Project");
+		projectForm.add(projects);		
+		
+		name.setFieldLabel("Title");
+		name.setReadOnly(true);
+		name.setStyleAttribute("color", "grey");
+		projectForm.add(name, fd);
+
+		pi.setFieldLabel("PI");
+		pi.setReadOnly(true);
+		pi.setStyleAttribute("color", "grey");
+		projectForm.add(pi, fd);
+		
+		coi.setFieldLabel("COI");
+		coi.setReadOnly(true);
+		coi.setStyleAttribute("color", "grey");
+		projectForm.add(coi, fd);
+		
+		obsNotes.setFieldLabel("Observer Notes");
+		projectForm.add(obsNotes, new FormData(500, 200));
+
+		schNotes.setFieldLabel("Scheduler Notes");
+		projectForm.add(schNotes, new FormData(500, 200));
+		
+	   	reset.setText("Reset");
+    	projectForm.add(reset);
+    	
+
+	   	save.setText("Save");
+    	projectForm.add(save);
+    	
+		add(projectForm);
+	}
+
+	private void initListeners() {
+		
+		// when a project gets picked, populate the sessions combo
+		projects.addListener(Events.Valid, new Listener<BaseEvent>() {
+		  	public void handleEvent(BaseEvent be) {
+		  		// go git it
+		  		getProject(projects.getSimpleValue());
+		   	}
+		});		
+		
+		obsNotes.addListener(Events.Blur, new Listener<BaseEvent>() {
+			@Override
+			public void handleEvent(BaseEvent be) {
+	            markTextAreaChange(be);
+			}			
+    	});			
+		
+		schNotes.addListener(Events.Blur, new Listener<BaseEvent>() {
+			@Override
+			public void handleEvent(BaseEvent be) {
+	            markTextAreaChange(be);
+			}			
+    	});		
+		
+    	reset.addListener(Events.OnClick, new Listener<BaseEvent>() {
+    		public void handleEvent(BaseEvent be) {
+    			getProject(projects.getSimpleValue());
+    		}
+    	});
+    	
+    	save.addListener(Events.OnClick, new Listener<BaseEvent>() {
+    		public void handleEvent(BaseEvent be) {
+    			updateProject(projects.getSimpleValue());
+    		}
+    	});
+		
+	}
+	
+	// if the textarea changes value, mark it in red
+	private void markTextAreaChange(BaseEvent be) {
+        String value = ((TextArea) be.getSource()).getValue();
+        String orgvl = ((TextArea) be.getSource()).getOriginalValue();
+        if (orgvl.compareTo(value) == 0) {
+        	((TextArea) be.getSource()).setStyleAttribute("color", "black");
+        } else {
+        	((TextArea) be.getSource()).setStyleAttribute("color", "red");
+        }
+	}
+
+	// retrieves the project's JSON
+	private void getProject(String pcode) {
+		String url = "/projects/" + project_ids.get(pcode);
+		HashMap <String, Object> keys = new HashMap<String, Object>();
+		
+		JSONRequest.get(url, keys, new JSONCallbackAdapter() {
+			// this url returns all the time accounting for the whole proj., 
+			// so use it to update the whole UI
+			public void onSuccess(JSONObject json) {
+				projectJson = json;
+				populateProjectPage(json);
+			}
+		});		
+		
+	}
+	
+	// populates this page's widgets with the project's JSON values
+	protected void populateProjectPage(JSONObject json) {
+		
+		JSONObject proj = json.get("project").isObject();
+		GWT.log("name: " + proj.get("name").toString(), null);
+		
+		// read-only fields
+	    name.setValue(proj.get("name").isString().stringValue());
+	    pi.setValue(proj.get("pi").isString().stringValue());
+	    coi.setValue(proj.get("co_i").isString().stringValue());
+	    
+	    // for writable fields, set them up again so that changes
+	    // can be marked w/ red.
+	    String notes = proj.get("schd_notes").isString().stringValue();
+	    schNotes.setValue(notes); 
+	    schNotes.setOriginalValue(notes);
+	    schNotes.setStyleAttribute("color", "black");
+
+	    notes = proj.get("notes").isString().stringValue();
+	    obsNotes.setValue(notes);
+	    obsNotes.setOriginalValue(notes);
+	    obsNotes.setStyleAttribute("color", "black");
+		
+	}
+
+	// gets all project codes form the server and populates the project combo
+	public void updatePCodeOptions() {
+		JSONRequest.get("/sessions/options"
+			      , new HashMap<String, Object>() {{
+			    	  put("mode", "project_codes");
+			        }}
+			      , new JSONCallbackAdapter() {
+			public void onSuccess(JSONObject json) {
+				// get ready to populate the project codes list
+				projects.removeAll();
+				project_ids.clear();
+				JSONArray pcodes = json.get("project codes").isArray();
+				JSONArray ids    = json.get("project ids").isArray();
+				for (int i = 0; i < pcodes.size(); ++i){
+					String pcode = pcodes.get(i).toString().replace('"', ' ').trim();
+					int id = (int) ids.get(i).isNumber().doubleValue();
+					project_ids.put(pcode, id);
+					projects.add(pcode);
+					
+				}
+			}
+		});
+	}
+
+	// take changes from widgets and send them over to the server to change
+	// this project
+	private void updateProject(String pcode) {
+		// first we have to transfer over the original json we got from the server
+		// to a HashMap - except for fields we're changing
+		String value;
+		HashMap<String, Object> keys = new HashMap<String, Object>();
+		// necessary for triggering an update on the server
+		keys.put("_method", "put"); 
+		JSONObject json = projectJson.get("project").isObject();
+		Set<String> set = json.keySet();
+		for (String key : set) {
+			// avoid the fields we're changing
+			if ((!key.equals("notes")) && (!key.equals("shcd_notes"))) {
+				GWT.log("iterator: "+ key + " : " + json.get(key).toString(), null);
+				if (json.get(key).isString() != null) {
+					value = json.get(key).isString().stringValue();
+				} else {
+					value = json.get(key).toString();
+				}
+				keys.put(key, value);
+			}
+			// now set the fields we're changing
+			keys.put("notes", obsNotes.getValue());
+			keys.put("schd_notes", schNotes.getValue());			
+			
+		}
+        
+		JSONRequest.post("/projects/" + project_ids.get(pcode), keys,
+			      new JSONCallbackAdapter() {
+			public void onSuccess(JSONObject json) {
+	                GWT.log("updateProject onSuccess", null);
+	                // refresh the project from the server
+	                getProject(projects.getSimpleValue());
+				}
+		    });
+	}
+
+	public void setProject(String pcode) {
+		GWT.log("PP.setProject", null);
+		projects.setSimpleValue(pcode);
+	
+	}		
+}
