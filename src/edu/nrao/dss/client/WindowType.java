992cee460cc60c50aadfc2dca29a20e4bae1e499
Paul Marganian <pmargani@nrao.edu>
2010-02-01 10:35:19 -0500

    Window Explorer now works properly with paging

--- src/edu/nrao/dss/client/WindowType.java
+++ src/edu/nrao/dss/client/WindowType.java
@@ -5,6 +5,7 @@ import com.extjs.gxt.ui.client.data.ModelType;
 public class WindowType extends ModelType {
 	public WindowType(ColumnType[] columnTypes) {
 		setRoot("windows");
+        this.setTotalName("total");
 		
         addField("id");
         for (ColumnType ct : columnTypes) {
